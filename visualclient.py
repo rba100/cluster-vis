001dc46f8df7d82255c5d856126dca89c26fee07
Robin Anderson <robin@rbanderson.co.uk>
2023-11-03 14:31:29 +0000

    Update visualclient.py

--- visualclient.py
+++ visualclient.py
@@ -60,7 +60,7 @@ def render_tsne_plotly(xtsne, labels, lines, label_descriptions):
         title='t-SNE visualization',
         autosize=False,
         width=800,
-        height=1000,
+        height=800,
         margin=dict(l=50, r=50, b=100, t=100, pad=4),
         legend=dict(
             title='Cluster Descriptions',
