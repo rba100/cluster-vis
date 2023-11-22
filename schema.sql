ed8bd78345537771f6c4515e32b9ee7acbbe2d5c
Robin Anderson <robin@rbanderson.co.uk>
2023-11-22 11:33:16 +0000

    Update schema.sql

--- schema.sql
+++ schema.sql
@@ -1,13 +1,20 @@
---CREATE TABLE words (word varchar(64) PRIMARY KEY, embedding vector(1536) NOT NULL, isCommon boolean NOT NULL DEFAULT false);
---CREATE TABLE ada_cache (input text PRIMARY KEY, embedding vector(1536) NOT NULL);
---CREATE INDEX idx_vec_words_cs ON words USING hnsw (embedding vector_cosine_ops) WITH (m = 16, ef_construction = 64);
---CREATE INDEX idx_vec_words_cs ON words USING hnsw (embedding vector_cosine_ops) WITH (m = 32, ef_construction = 120);
---SELECT indexname FROM pg_indexes WHERE tablename = 'words';
+SELECT 'read the script' FROM you_pressed_f5_without_reading_the_script;
+
+-- Do not run this whole script. Select individual statements and run
+-- them one at a time.
+
+-- install the pgvector extension, minimum version 0.51.0
+
+CREATE TABLE words (word varchar(64) PRIMARY KEY, embedding vector(1536) NOT NULL, isCommon boolean NOT NULL DEFAULT false);
+
+-- Do not build the index until you have populated the words table.
+-- Note: This takes a long time, leave it going overnight.
+CREATE INDEX idx_vec_words_cs ON words USING hnsw (embedding vector_cosine_ops) WITH (m = 32, ef_construction = 120);
 
 CREATE TABLE ada_cache2 (
-  id SERIAL PRIMARY KEY,                -- A unique identifier for each row
-  hash CHAR(32) NOT NULL UNIQUE,   -- MD5 hash of the text
-  embedding vector(1536) NOT NULL       -- The embedding vector of size 1536
+  id SERIAL PRIMARY KEY,
+  hash CHAR(32) NOT NULL UNIQUE,  -- MD5 hash of the text (not used for security; keep your hair on)
+  embedding vector(1536) NOT NULL -- The embedding vector
 );
 
 CREATE INDEX idx_ada_cache2_hash ON ada_cache2 USING btree (hash);
